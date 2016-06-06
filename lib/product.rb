a1e99100c720f422ad1b5f4f0c773cf3c3a69983
Tianqing <442034859@qq.com>
2016-06-06 15:04:15 +0800

    DuplicateProductError Function done

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,26 @@
 class Product
   @@products = []
 
+  attr_reader :title
+
   def initialize (options = {})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
   end
+
+  private
+
+  def add_to_products
+     @@products.each do |product|
+       if product.title == @title
+         raise DuplicateProductError, "DuplicateProduct with same title"
+       end
+     end
+     @@products << self
+  end
+
 end
